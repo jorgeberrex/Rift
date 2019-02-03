e2a2befd9be9692bf29dac9b678d20b34ecf2f36
Unknown <Chocohead@Aroma1997.org>
2019-02-03 03:41:12 +0000

    Fix armour texture Mixin missing

--- src/main/java/org/dimdev/rift/mixin/core/client/MixinLayerArmorBase.java
+++ src/main/java/org/dimdev/rift/mixin/core/client/MixinLayerArmorBase.java
@@ -1,26 +1,40 @@
 package org.dimdev.rift.mixin.core.client;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import net.minecraft.client.renderer.entity.model.ModelBase;
 import net.minecraft.util.ResourceLocation;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(LayerArmorBase.class)
 public class MixinLayerArmorBase<T extends ModelBase> {
-    @Redirect(
+	private static final Pattern TEXTURE_MASK = Pattern.compile("textures\\/models\\/armor\\/(.+)(_layer_\\d\\w*\\.png)");
+
+    @ModifyArg(
         method = "getArmorResource(Lnet/minecraft/item/ItemArmor;ZLjava/lang/String;)Lnet/minecraft/util/ResourceLocation;",
         at = @At(
             value = "INVOKE",
-            target = "Ljava/lang/String;format(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"
-        )
+            target = "Ljava/util/Map;computeIfAbsent(Ljava/lang/Object;Ljava/util/function/Function;)Ljava/lang/Object;",
+            remap = false
+        ),
+        index = 0
     )
-    private String getArmorTexture(String format, Object... args) {
-        String name = (String) args[0];
-        int layer = (int) args[1];
-        String typeSuffix = (String) args[2];
+    private Object getArmorTexture(Object old) {
+    	Matcher match = TEXTURE_MASK.matcher((String) old);
+    	if (!match.matches() || match.groupCount() != 2) {
+    		//Strange texture we can't resolve
+    		throw new IllegalArgumentException("Unexpected armor texture: " + old);
+    	}
+
+    	String name = match.group(1);
+    	String extra = match.group(2);
         ResourceLocation location = new ResourceLocation(name);
-        return String.format("%s:textures/models/armor/%s_layer_%d%s.png", location.getNamespace(), location.getPath(), layer, typeSuffix);
+
+        return location.getNamespace() + ":textures/models/armor/" + location.getPath() + extra;
     }
 }
