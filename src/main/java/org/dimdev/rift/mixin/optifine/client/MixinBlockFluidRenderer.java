e5e5e38871c6e4dc8153cbedc19fed041f7bb879
Unknown <Chocohead@Aroma1997.org>
2019-02-03 03:41:34 +0000

    Fix Optifine fluid colour Mixin not working

--- src/main/java/org/dimdev/rift/mixin/optifine/client/MixinBlockFluidRenderer.java
+++ src/main/java/org/dimdev/rift/mixin/optifine/client/MixinBlockFluidRenderer.java
@@ -13,7 +13,6 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 
 import org.dimdev.rift.injectedmethods.RiftFluid;
 
@@ -37,15 +36,5 @@ public class MixinBlockFluidRenderer {
 		} else {
 			return new TextureAtlasSprite[] {MissingTextureSprite.getSprite(), MissingTextureSprite.getSprite()};
 		}
-	}
-
-	@Redirect(at = @At(value = "INVOKE_ASSIGN", target = "Lnet/optifine/CustomColors;getFluidColor(Lnet/minecraft/world/IWorldReader;Lnet/minecraft/block/state/IBlockState;Lnet/minecraft/util/math/BlockPos;)I", shift = Shift.BEFORE, remap = false), method = "render(Lnet/minecraft/world/IWorldReader;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/client/renderer/BufferBuilder;Lnet/minecraft/fluid/IFluidState;)Z", constraints = "OPTIFINE(1+)")
-	public int getFluidColours(BlockFluidRenderer me, int optifine, IWorldReader world, BlockPos pos, BufferBuilder buffer, IFluidState state) {
-		if (state.getFluid() instanceof RiftFluid) {
-			RiftFluid fluid = (RiftFluid) state.getFluid();
-			if (fluid.ignoreOptifine()) return fluid.getColorMultiplier(world, pos);
-		}
-
-		return optifine;
 	}
 }
